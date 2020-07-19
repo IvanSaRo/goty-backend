a8320505f8fe3ee5b34b05688fa67ae594cb3212
IvanSaRo <ivansancrodr@gmail.com>
2020-07-20 00:35:09 +0200

    Express - Cors installed @types for both, getGoty works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,19 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+import * as express from "express";
+import * as cors from "cors";
 
 
+const serviceAccount = require("./serviceAccountKey.json")
 
-var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://firestore-graph-85545.firebaseio.com"
-});
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-graph-85545.firebaseio.com"
+  });
 
-
-
-
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -21,3 +21,17 @@ admin.initializeApp({
    functions.logger.info("Hello logs!", {structuredData: true});
    response.json({mensaje: "Hola mundo desde funciones!"});
  });
+
+ export const getGOTY = functions.https.onRequest(async (request, response) => {
+    
+    const gotyRef  = db.collection("goty");
+    const docsSnap = await gotyRef.get();
+    const games    = docsSnap.docs.map( doc => doc.data() );
+
+    response.json( games );
+  });
+
+ 
+ //Express
+
+ 
