56923c7257c4fc039f6f18a6ec2cba3fecf39192
IvanSaRo <ivansancrodr@gmail.com>
2020-07-19 19:34:26 +0200

    service account key added, localhost:5000 ok, tsc --watch runs well

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+
+
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://firestore-graph-85545.firebaseio.com"
+});
+
+
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
  export const helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+   response.json({mensaje: "Hola mundo desde funciones!"});
  });
